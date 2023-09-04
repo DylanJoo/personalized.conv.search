970bfc7feac639b972248ae9bd424e6ea7415a66
DylanJoo <dylanjootw@gmail.com>
2023-09-04 10:51:08 +0800

    add config

--- move.sh
+++ move.sh
@@ -1,6 +1,4 @@
-cp -r models/ckpt/start-contriever-ms-B160-A0.1/checkpoint-20000/
+cp -r models/ckpt/start-contriever-ms-B160-A0.1/checkpoint-20000/ \
+    /home/jhju/personlized_/home/jhju/personalized.conv.search/models/ckpt/
+
 cp -r data/ikat/ikat.test.t5ntr_history_3-3.jsonl \
-	    --device cuda:2 \
-	    --concat_ptkb \
-	    --batch_size 16
-	# [NOTE] ikat-subtask3 required.
