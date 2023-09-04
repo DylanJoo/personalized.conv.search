d5af7519c6763ecfa548f0a48fe372c964b5e5b0
DylanJoo <dylanjootw@gmail.com>
2023-09-04 11:34:31 +0800

    remove moving scripts

--- move.sh
+++
@@ -1,4 +0,0 @@
-cp -r models/ckpt/start-contriever-ms-B160-A0.1/checkpoint-20000/ \
-    /home/jhju/personlized_/home/jhju/personalized.conv.search/models/ckpt/
-
-cp -r data/ikat/ikat.test.t5ntr_history_3-3.jsonl \
